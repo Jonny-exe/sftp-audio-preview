58be6415063a213ab2823c4ab13f1bde02c7e0b3
jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-09-25 22:20:38 +0200

    now everything working. I added a little files viewer and polished everything

--- gui.py
+++ gui.py
@@ -1,17 +1,36 @@
 #!/usr/bin/python3
-from main import connect_and_getfile, load_cache, save_cache, play
+from main import connect_sftp, load_cache, save_cache, play, get_file
 import os
 from os.path import exists
+from copy import deepcopy
+from functools import partial
 import sys
-from PySide2.QtWidgets import (QLineEdit, QPushButton, QApplication,
-    QVBoxLayout, QDialog, QLabel, QCheckBox)
+from PySide2.QtWidgets import (
+    QLineEdit,
+    QPushButton,
+    QApplication,
+    QVBoxLayout,
+    QDialog,
+    QLabel,
+    QCheckBox,
+    QScrollArea,
+    QWidget,
+    QMainWindow,
+    QButtonGroup,
+    QGroupBox
+)
+from PySide2.QtCore import Qt, QSize
 
-class Form(QDialog):
 
+class Form(QMainWindow):
     def __init__(self, parent=None):
         super(Form, self).__init__(parent)
         self.setGeometry(500, 500, 500, 500)
         self.p = None
+
+        self.scroll = QScrollArea()
+        self.widget = QWidget()
+
         # Create widgets
         url, user, password, filename, remove_file = load_cache()
         self.text = QLabel("Play audio files through ftp")
@@ -23,10 +42,14 @@ class Form(QDialog):
         self.remove_file = QCheckBox("Remove file afterwards")
         self.button_connect = QPushButton("Connect and get file")
         self.button_stop = QPushButton("Stop audio")
+        self.files_container = QGroupBox()
+        self.files = QVBoxLayout()
+        self.files_container.setLayout(self.files)
         # Create layout and add widgets
 
         self.layout = QVBoxLayout()
         self.layout.addWidget(self.text)
+
         # self.layout.addWidget(self.text_status)
         self.layout.addWidget(self.edit_url)
         self.layout.addWidget(self.edit_user)
@@ -34,6 +57,7 @@ class Form(QDialog):
         self.layout.addWidget(self.edit_filename)
         self.layout.addWidget(self.remove_file)
         self.layout.addWidget(self.button_connect)
+        self.layout.addWidget(self.files_container)
 
         self.edit_url.setPlaceholderText("url")
         self.edit_user.setPlaceholderText("user")
@@ -45,6 +69,13 @@ class Form(QDialog):
         self.button_stop.clicked.connect(self.stop)
         self.setLayout(self.layout)
 
+        # Scroll Area Properties
+        self.widget.setLayout(self.layout)
+        self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.scroll.setWidgetResizable(True)
+        self.scroll.setWidget(self.widget)
+        self.setCentralWidget(self.scroll)
 
     def closeEvent(self, event):
         if self.remove_file.isChecked():
@@ -56,34 +87,66 @@ class Form(QDialog):
             pass
         event.accept()
 
-
     # Greets the user
     def connect(self):
         url = self.edit_url.text()
         user = self.edit_user.text()
         password = self.edit_password.text()
-        filename = self.edit_filename.text()
+        # filename = self.edit_filename.text()
         remove_file = self.remove_file.isChecked()
-        response = connect_and_getfile(url, user, password, filename, remove_file)
-        if isinstance(response, Exception):
+        # response = connect_and_getfile(url, user, password, remove_file)
+        conn = connect_sftp(url, user, password, remove_file)
+        if isinstance(conn, Exception):
             self.close()
             return
+        dirs = self.add_dirs(conn)
 
+    def get_and_play(self, conn, filename):
+        print("Get and play", filename)
+         
+        get_file(conn, filename)
         self.p = play()
         self.layout.addWidget(self.button_stop)
+    
+    def add_dirs(self, conn, path=None):
+        if path is not None:
+            conn.chdir(path)
+        dirs = conn.listdir(conn.pwd)
+
+        while self.files.count() != 0:
+            widget = self.files.takeAt(0)
+            widget.widget().hide()
+        self.button_back = QPushButton("..")
+        self.files.addWidget(self.button_back)
+        self.button_back.clicked.connect(partial(self.add_dirs, conn, path=".."))
+
+        print(self.files.count())
+        for folder_name in dirs:
+            isdir = conn.isdir(folder_name)
+            label = QPushButton(folder_name + " * " * int(isdir))
+            if isdir:
+                func = partial(self.add_dirs, conn, path=conn.pwd+"/"+folder_name)
+            else:
+                func = partial(self.get_and_play, conn, conn.pwd+"/"+folder_name)
+            label.clicked.connect(func)
+                
+            self.files.addWidget(label)
+            
+        return dirs
 
     def stop(self):
         if self.p is not None:
             self.p.terminate()
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Create the Qt Application
     app = QApplication(sys.argv)
     # Create and show the form
-    form = Form()
-    form.show()
-    # Run the main Qt loop
-    sys.exit(app.exec_())
-
+    try:
+        form = Form()
+        form.show()
+        # Run the main Qt loop
+        sys.exit(app.exec_())
+    except KeyboardInterrupt:
+        print("Bye")
--- main.py
+++ main.py
@@ -9,53 +9,53 @@ import multiprocessing
 import pysftp
 
 CACHE_FILE = "cache.json"
-logging.basicConfig(filename='gui.log', level=logging.DEBUG)
- 
-def connect_and_getfile(url, user, password, filename, remove_file):
+logging.basicConfig(filename="gui.log", level=logging.DEBUG)
 
 
-    sftp = pysftp.Connection(url, username=user, password=password)
-    sftp.get(filename, "audio")
-    """
-    try:
-        ftpObject = FTP(url);
-    except Exception:
-        error = "Error connecting to server. Url is wrong or server is down"
-        logging.error(error)
-        return Exception(error)
+def connect_sftp(url, user, password, remove_file):
+    conn = pysftp.Connection(url, username=user, password=password, cnopts=None)
+    conn.cd("~")
+    # sftp.get(filename, "audio")
+    
 
-    ftpObject.login(user, password);
-    ftpObject.retrbinary(f"RETR {filename}", open("audio", 'wb').write);
-    """
+    # save_cache(url, user, password, "audio.m4a", remove_file)
+    return conn
+    
+def get_file(conn, filename):
+    conn.get(filename, "audio")
 
-    save_cache(url, user, password, filename, remove_file)
 
 def play():
-    p = multiprocessing.Process(target=playsound, args=('audio', ))
+    p = multiprocessing.Process(target=playsound, args=("audio",))
     print(p)
     p.start()
     return p
 
+
 def save_cache(url, user, password, filename, remove_file):
     obj = {
-            "url": url,
-            "user": user,
-            "password": password,
-            "filename": filename,
-            "remove_file": remove_file
-        }
+        "url": url,
+        "user": user,
+        "password": password,
+        "filename": filename,
+        "remove_file": remove_file,
+    }
 
     with open(CACHE_FILE, "w") as outfile:
-            json.dump(obj, outfile)
+        json.dump(obj, outfile)
+
 
 def load_cache():
     if not exists(CACHE_FILE):
         return "", "", "", "", True
-    with open(CACHE_FILE, 'r') as openfile:
+    with open(CACHE_FILE, "r") as openfile:
         obj = json.load(openfile)
 
-    url, user, password, filename, remove_file = obj["url"], obj["user"], obj["password"], obj["filename"], obj["remove_file"]
+    url, user, password, filename, remove_file = (
+        obj["url"],
+        obj["user"],
+        obj["password"],
+        obj["filename"],
+        obj["remove_file"],
+    )
     return url, user, password, filename, remove_file
-
-
-
